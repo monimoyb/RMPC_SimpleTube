41e4a619cbd06f8ad00dfc029208ec2a2c1609c3
Monimoy Bujarbaruah <monimoy_07@yahoo.co.in>
2021-04-23 03:56:49 -0700

    Update FTOCP_addTime.m
    cases fixed

--- FTOCP_addTime.m
+++ FTOCP_addTime.m
@@ -1,45 +1,36 @@
 %% Finite time optimal control problem: This one used in the time computation part
-% Monimoy 
+% Monimoy Bujarbaruah
 
 
 function [feas_flag, cost_flag, v_hor, sol_time] = FTOCP_addTime(x_0, Q, R, Pinf, Anom, Bnom, N, X, U, Xn, setdelA, setdelB, W, W_Term, nx, nu, dim_t, matF, matG, matH, mat_c)
 
-  options = sdpsettings('solver','gurobi','verbose',0);
+    options = sdpsettings('solver','gurobi','verbose',0);
  
-  %% solve the opt problem  
-  constraints = []; 
+    %% solve the opt problem  
+    constraints = []; 
   
-   %% Creating Open Loop Optimization Variables for MPC 
+    %% Creating Open Loop Optimization Variables for MPC 
+    v = sdpvar(nu*N,1);    
+    x0feas = x_0;
     
-    M = sdpvar(nu*N,nx*N);        
-        for j=1:nu*N
-            for k = 2*j-1:nx*N
-                    M(j,k) =0;
-            end
-        end        
-     v = sdpvar(nu*N,1);    
-     x0feas = x_0;
-    
-     %% Build the cost 
+    %% Build the cost 
     x_pred = sdpvar(nx,N+1);
     x_pred(:,1) = x0feas; 
     
-    cost_state = (x_pred(:,1))'*Q*(x_pred(:,1)); 
-    
+    cost_state = (x_pred(:,1))'*Q*(x_pred(:,1));    
     for k=1:N
         x_pred(:,k+1)  = Anom*x_pred(:,k) + Bnom*v(1+(k-1)*nu:k*nu,1);
         if k~=N
-        cost_state = cost_state + (x_pred(:,k))'*Q*(x_pred(:,k));
+            cost_state = cost_state + (x_pred(:,k))'*Q*(x_pred(:,k));
         end
     end
-    cost_state = cost_state + (x_pred(:,N+1))'*Pinf*(x_pred(:,N+1));
     
+    cost_state = cost_state + (x_pred(:,N+1))'*Pinf*(x_pred(:,N+1));   
     obj_ol = v'*kron(R,N)*v + cost_state; 
      
-   %% Solving the Optimization Problem 
-   %%% Cleanly separate the cases here. %%%%%%
+    %% Cleanly separate the cases here. %%%%%%
    
-   if N ==1 
+    if N ==1 
         boldAbar = kron(eye(N),Anom);
         boldBbar = kron(eye(N),Bnom); 
         Fx = blkdiag(kron(eye(N-1), X.A), Xn.A); 
@@ -50,13 +41,10 @@ function [feas_flag, cost_flag, v_hor, sol_time] = FTOCP_addTime(x_0, Q, R, Pinf
         boldhu = kron(ones(N,1), U.b); 
         %%%variables 
         Lambda = sdpvar(size(Fx,1), size(boldHw,1), 'full'); 
-        constraints = [constraints; Lambda>=0];
-        gamma = sdpvar(size(boldHw,1), size(boldHu,1), 'full'); 
-        constraints = [constraints; gamma >=0];
-        constraints = [constraints; gamma'*boldhw <= boldhu - boldHu*v];      
-        constraints = [constraints; (boldHu*M)' == boldHw'*gamma];                  %   input con
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %%% enumerate the set of vertices here 
+        constraints = [constraints; Lambda>=0];  
+        constraints = [constraints; boldHu*v <= boldhu];                 % input con
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%% enumerate the set of vertices here 
         for ii = 1:size(setdelA,3)
             bolddelA = kron(eye(N),setdelA(:,:,ii));
             for jj = 1:size(setdelB,3) 
@@ -66,9 +54,15 @@ function [feas_flag, cost_flag, v_hor, sol_time] = FTOCP_addTime(x_0, Q, R, Pinf
             end
         end
    
-        constraints = [constraints; Lambda*boldHw == Fx];                          % state con
+        constraints = [constraints; Lambda*boldHw == Fx];                % state con
    
-   else      
+    else        
+        M = sdpvar(nu*N,nx*N);        
+        for j=1:nu*N
+            for k = 2*j-1:nx*N
+                M(j,k) =0;
+            end
+        end  
         %%  Open Loop W Variables  
         Hs=[]; hs =[];   
             for k = 1:N
@@ -81,16 +75,15 @@ function [feas_flag, cost_flag, v_hor, sol_time] = FTOCP_addTime(x_0, Q, R, Pinf
         Z = sdpvar(dim_a,dim_t);  
         constraints = [constraints; matF*v + Z'*hs <= mat_c + matH*x0feas];
         constraints = [constraints; Z>=0];
-        constraints = [constraints, matF*M + matG == Z'*Hs];                       % joint con (see www.sciencedirect.com/science/article/pii/S0005109806000021)
-   end
+        constraints = [constraints, matF*M + matG == Z'*Hs];             % joint con (see www.sciencedirect.com/science/article/pii/S0005109806000021)
+    end
    
-   %% Optimize 
+    %% Optimize 
     diagn=solvesdp(constraints, obj_ol, options);
     
     sol_time = diagn.solvertime; 
     feas_flag = diagn.problem; 
    
-    
     if feas_flag ~=0
         cost_flag = inf;                       % store high cost if any issue
         v_hor = zeros(nu*N,1);                 % just store some dummy 0's if infeasible anyway
