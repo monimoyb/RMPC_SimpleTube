3e451ce905efd0090c6a298cbf30286f2781cbf5
Monimoy Bujarbaruah <monimoy_07@yahoo.co.in>
2021-04-23 04:06:37 -0700

    Update term_setRobPar.m
    indents fixed

--- term_setRobPar.m
+++ term_setRobPar.m
@@ -1,42 +1,42 @@
 %% Calculating the Terminal Set X_N of Robust MPC
-% Monimoy Bujarbaruah
+%  Monimoy Bujarbaruah
 
 function [Xn,Pinf] = term_setRobPar(Anom, Bnom, delAv, delBv, K, X, U, W, Q, R, nx, nu)
 
-Pinf = dlyap((Anom-Bnom*K)',Q+K'*R*K);                         %  Cost based on dlyap as K is fixed 
+    Pinf = dlyap((Anom-Bnom*K)',Q+K'*R*K);                         %  Cost based on dlyap as K is fixed 
 
-%%% extract the set of possible matrices%%%% 
-setA = zeros(nx, nx, size(delAv,2)/nx);
-setB = zeros(nx, nu, size(delBv,2)/nu);
+    %%% extract the set of possible matrices%%%% 
+    setA = zeros(nx, nx, size(delAv,2)/nx);
+    setB = zeros(nx, nu, size(delBv,2)/nu);
 
-for i = 1:size(delAv,2)/nx
-    setA(:,:,i) = Anom + delAv(:,(i-1)*nx + 1: i*nx);  
-end
-
-for i = 1:size(delBv,2)/nu
-    setB(:,:,i) = Bnom + delBv(:,(i-1)*nu + 1: i*nu);  
-end
-
-%%%%%%%% form all possible pairs %%%%%%
-cl_mat = zeros(nx,nx, (size(delAv,2)/nx)*(size(delBv,2)/nu));
-count = 0;
-for i = 1:(size(delAv,2)/nx)
-    for j = 1: size(delBv,2)/nu
-        count = count + 1;
-        cl_mat(:,:,count) = setA(:,:,i) - setB(:,:,j)*K;
+    for i = 1:size(delAv,2)/nx
+        setA(:,:,i) = Anom + delAv(:,(i-1)*nx + 1: i*nx);  
     end
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%% check stability
-for i = 1: size(cl_mat,3)
-   max_abs(i) =  max(abs(eig(cl_mat(:,:,i)))); 
-end
-%%%%%%
+    for i = 1:size(delBv,2)/nu
+        setB(:,:,i) = Bnom + delBv(:,(i-1)*nu + 1: i*nu);  
+    end
 
-S = Polyhedron('A',X.A,'b',X.b); 
-S = S.intersect(Polyhedron('H',[-U.H(:,1:nu)*K U.H(:,nu+1)])); 
+    %%%%%%%% form all possible pairs %%%%%%
+    cl_mat = zeros(nx,nx, (size(delAv,2)/nx)*(size(delBv,2)/nu));
+    count = 0;
+    for i = 1:(size(delAv,2)/nx)
+        for j = 1: size(delBv,2)/nu
+            count = count + 1;
+            cl_mat(:,:,count) = setA(:,:,i) - setB(:,:,j)*K;
+        end
+    end
+    %%%%%%%%%%%%%%%%%%%%%%%%%
 
-Xn = MRPI_Par(cl_mat, S, Polyhedron.emptySet(nx), W);
+    %%% check stability vertex-wise (must be stable)
+    for i = 1: size(cl_mat,3)
+        max_abs(i) =  max(abs(eig(cl_mat(:,:,i)))); 
+    end
+    %%%%%%
+
+    S = Polyhedron('A',X.A,'b',X.b); 
+    S = S.intersect(Polyhedron('H',[-U.H(:,1:nu)*K U.H(:,nu+1)])); 
+
+    Xn = MRPI_Par(cl_mat, S, Polyhedron.emptySet(nx), W);
 
 end
